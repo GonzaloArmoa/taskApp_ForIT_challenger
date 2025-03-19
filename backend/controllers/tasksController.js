0d5a024e54eaa24a21d2b8a666cec2fdf311427c
Gonzalo Armoa <garmoa.dev@gmail.com>
2025-03-18 21:27:04 -0300

    Se agrega funcion list & create en tasksController

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -1,44 +1,63 @@
+const { unlinkSync, existsSync } = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+const { readJSON, writeJSON } = require("../data");
+
 module.exports = {
 
-    list: function(req, res) {
-        res.status(200).json({
-          ok:true,
-          msg:"Lista de Tareas!!"
-        });
-    },
+  list: function (req, res) {
+    const products = readJSON("tareas.json");
+    res.status(200).json({
+      ok: true,
+      msg: "Listado de Tareas",
+      data: products
+    });
+  },
 
-    create: function(req, res) {
-        res.status(200).json({
-          ok:true,
-          msg:"Se ha añadido la tarea a la lista"
-        });
-    },
+  create: function (req, res) {
+    const products = readJSON("tareas.json");
 
-    detail: function(req, res) {
-        res.status(200).json({
-          ok:true,
-          msg:"Datalle de la Tarea"
-        });
-    },
+    products.push({
+      id: uuidv4(),
+      titulo: req.body.titulo,
+      estado: "pendiente",
+      fecha: new Date().toISOString().split('T')[0]
+    });
 
-    update: function(req, res) {
-        res.status(200).json({
-          ok:true,
-          msg:"Se ha actualizado la tarea"
-        });
-    },
+    writeJSON(products, "tareas.json");
 
-    remove: function(req, res) {
-        res.status(200).json({
-          ok:true,
-          msg:"Tarea eliminada de la lista"
-        });
-    },
+    res.status(200).json({
+      ok: true,
+      msg: "Se ha añadido la tarea a la lista",
+      data: products
+    });
+  },
 
-    changeState: function(req, res, next) {
-        res.status(200).json({
-          ok:true,
-          msg:"Se ha actualizado el estado de la tarea"
-        });
-    },
+  detail: function (req, res) {
+    res.status(200).json({
+      ok: true,
+      msg: "Datalle de la Tarea"
+    });
+  },
+
+  update: function (req, res) {
+    res.status(200).json({
+      ok: true,
+      msg: "Se ha actualizado la tarea"
+    });
+  },
+
+  remove: function (req, res) {
+    res.status(200).json({
+      ok: true,
+      msg: "Tarea eliminada de la lista"
+    });
+  },
+
+  changeState: function (req, res, next) {
+    res.status(200).json({
+      ok: true,
+      msg: "Se ha actualizado el estado de la tarea"
+    });
+  },
 } 
--- backend/data/index.js
+++ backend/data/index.js
@@ -2,6 +2,6 @@ const {readFileSync, writeFileSync} = require('fs');
 const path = require('path');
 
 module.exports = {
-    leerJSON : () => JSON.parse(readFileSync(path.join(__dirname,'tareas.json'),'utf-8')),
-    escribirJSON : (tareas) => writeFileSync(path.join(__dirname,'tareas.json'),JSON.stringify(tareas,null,3),'utf-8')
+    readJSON : () => JSON.parse(readFileSync(path.join(__dirname,'tareas.json'),'utf-8')),
+    writeJSON : (tareas) => writeFileSync(path.join(__dirname,'tareas.json'),JSON.stringify(tareas,null,3),'utf-8')
 }
