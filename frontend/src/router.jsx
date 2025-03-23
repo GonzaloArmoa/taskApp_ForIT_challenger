568f0f61178334ad12264b693c829e639ebd815b
Gonzalo Armoa <garmoa.dev@gmail.com>
2025-03-23 19:55:22 -0300

    incorporacion de enrutador frontend

---
+++ frontend/src/router.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import App from './App';
+import Home from "./pages/Home";
+import TaskDetail from './pages/TaskDetail';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+
+      },
+      {
+        path: "/task/:id",
+        element: <TaskDetail />,
+      },
+    ],
+  },
+]);
